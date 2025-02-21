5027da0591cdcd9f1507d5296fa8e5ebde304909
Igor Koshkarev <igor.koshkarev.04@gmail.com>
2025-02-21 10:33:33 +0700

    create File class

---
+++ file.py
@@ -0,0 +1,9 @@
+
+class File:
+    def __init__(self, name: str, date: str, size: int):
+        self.name = name
+        self.date = date
+        self.size = size
+    
+    def print_all(self):
+        pass
