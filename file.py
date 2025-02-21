a0628b7fc00a772ff7a9794556ecb7c950f3cb98
Igor Koshkarev <igor.koshkarev.04@gmail.com>
2025-02-21 10:45:49 +0700

    create PDF file class

--- file.py
+++ file.py
@@ -1,9 +1,24 @@
+from abc import ABC, abstractmethod
 
-class File:
+class File(ABC):
     def __init__(self, name: str, date: str, size: int):
         self.name = name
         self.date = date
         self.size = size
     
+    @abstractmethod
     def print_all(self):
         pass
+
+class PDFFile(File):
+    def __init__(self, name, date, size, pages):
+        super().__init__(name, date, size)
+        self.type = "pdf"
+        self.pages = pages
+    
+    def print_all(self):
+        print('type: ', self.type)
+        print('name: ', self.name)
+        print('date: ', self.date)
+        print('size: ', self.size)
+        print('pages: ', self.size)
