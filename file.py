8aa6dd10e0641829435b895083003444b46a88e5
Igor Koshkarev <igor.koshkarev.04@gmail.com>
2025-02-21 10:50:27 +0700

    create PNG file class

--- file.py
+++ file.py
@@ -21,4 +21,21 @@ class PDFFile(File):
         print('name: ', self.name)
         print('date: ', self.date)
         print('size: ', self.size)
-        print('pages: ', self.size)
+        print('pages: ', self.pages)
+
+
+class PNGFile(File):
+    def __init__(self, name, date, size, width, height):
+        super().__init__(name, date, size)
+        self.type = 'png'
+        self.width = width
+        self.height = height
+    
+    def print_all(self):
+        print('type: ', self.type)
+        print('name: ', self.name)
+        print('date: ', self.date)
+        print('size: ', self.size)
+        print('pages: ', self.width)
+        print('pages: ', self.height)
+
