6b37383863faa89e575249c398f4042f102d88d8
Igor Koshkarev <igor.koshkarev.04@gmail.com>
2025-02-21 11:25:42 +0700

    add entry point

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ def get_files(filename: str) -> list[file.File]:
     with open(filename, 'r') as f:
         for i in f:
             info = i.split()
+            print(info)
             if info[0] == 'pdf':
-                files.append(file.PDFFile(*info))
+                files.append(file.PDFFile(*info[1:]))
             elif info[0] == 'png':
-                files.append(file.PNGFile(*info))
+                files.append(file.PNGFile(*info[1:]))
             else:
                 raise TypeError
     return files
 
+
+if __name__ == '__main__':
+    files = get_files('files.txt')
+    for f in files:
+        f.print_all()
+        print()
