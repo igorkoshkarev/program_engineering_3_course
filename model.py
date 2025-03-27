a69adb3fc87e3cec2b27bd88ec96835986817621
igorkoshkarev <igor.koshkarev.04@gmail.com>
2025-03-27 20:28:57 +0700

    create model class

---
+++ model.py
@@ -0,0 +1,24 @@
+from file import FILE_TYPE
+
+class Model:
+
+    ID = 0
+
+    def __init__(self):
+        self.files = {}
+
+    def create(self, file_type: FILE_TYPE, **params):
+        for i, v in params.items():
+            if isinstance(v, str):
+                assert v.strip() == "", "Есть незаполненные поля: " + i
+        
+        file = file_type.value.file_class(**params)
+        self.files[self.ID] = file
+        self.ID += 1
+
+    def remove(self, id):
+        self.files.pop(id)
+    
+    def select_all(self):
+        return self.files
+            
