618ded4dc14bdeeb817105d107220ee9a24efe9b
igorkoshkarev <igor.koshkarev.04@gmail.com>
2025-03-27 22:55:36 +0700

    create methods for MainController

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import file
+import sys
 from main_window import MainWindow
 from model import Model
+import create_file_window
+from PySide6.QtWidgets import QApplication
+
 
 def get_files(filename: str) -> list[file.File]:
     files = []
@@ -19,22 +23,46 @@ def get_files(filename: str) -> list[file.File]:
 
 class MainController:
 
+    CREATE_WINDOW_CLASS = {file.FILE_TYPE.PDF.name: create_file_window.CreatePDFFileWindow, 
+                           file.FILE_TYPE.PNG.name: create_file_window.CreatePNGFileWindow}
+
     def __init__(self):
+        self.app = QApplication(sys.argv)
         self.main_window = MainWindow()
         self.main_window.create_button.clicked.connect(self.view_create_window)
-        self.main_window.remove_button.clicked.connect(self.remove)
+        self.main_window.remove_button.clicked.connect(self.remove_elements)
         self.db = Model()
     
     def view_create_window(self):
-        pass
+        self.file_type = self.main_window.get_create_file_type()
+        self.create_window = self.CREATE_WINDOW_CLASS[self.file_type]()
+        self.create_window.create_button.clicked.connect(self.create)
+        self.create_window.show()
+    
+    def create(self):
+        file_type = self.create_window.get_file_type()
+        parameters = self.create_window.get_parameters()
+        id = self.db.create(file_type, **parameters)
+        self.main_window.add_row(id, file_type.value.name, parameters['name'], parameters['date'], parameters['size'])
+        self.create_window.close()
+        del self.create_window
 
-    def remove(self):
-        pass
+    def remove_elements(self):
+        selected_rows = self.main_window.selected_rows.copy()
+        for id in selected_rows:
+            self.remove_element(id)
+
+    def remove_element(self, id):
+        self.main_window.remove_row(id)
+        self.db.remove(id)
+    
+    def run(self):
+        self.main_window.show()
+        sys.exit(self.app.exec())
+
 
 
 
 if __name__ == '__main__':
-    files = get_files('files.txt')
-    for f in files:
-        f.print_all()
-        print()
+    m = MainController()
+    m.run()
